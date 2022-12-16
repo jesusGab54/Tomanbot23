66e140d8f4eea534e17a0a60a96ff0f7207f5458
jesusGab54 <114363032+jesusGab54@users.noreply.github.com>
2022-12-15 19:15:20 -0500

    Create config.py

---
+++ TomanBot/config.py
@@ -0,0 +1,26 @@
+from TomanBot.sample_config import Config
+
+class Development(Config):
+    OWNER_ID = 5516142677 # Su ID de telegram.
+    OWNER_USERNAME = "Remnashi" # Su nombre de usuario de telegram.
+    API_KEY = "DWPFBQ-MHGMCG-PRJUJY-NHSPWI-ARQ" # Su clave api, tal como la proporciona @botfather.
+    SQLALCHEMY_DATABASE_URI = 'postgres://dzpawnsj:2ZrIeM5vzxBhW8N4R6C_9nJTsZ2pWhcF@raja.db.elephant>
+    MESSAGE_DUMP = '-1001891927588' # Algún chat grupal donde su bot este ahí.
+    USE_MESSAGE_DUMP = True
+    SUDO_USERS = [5676170494, 5932340111] # Lista de identificadores de usuarios que tienen acceso s>
+    LOAD = []
+    NO_LOAD = ["rss", "cleaner", "connection", "math"]
+    DEV_USERS = [21396580, 954299697, 5010209835, 5676170494, 5492224551, 5516142677]
+    TIGER_USERS = [21396580, 954299697, 5010209835, 5676170494, 5492224551, 5516142677]
+    TOKEN="5911693677:AAFp4N42pdcC8qdI0y1OhRpSIi9idXxmyOE"
+    GBAN_LOGS = True
+    API_ID=22381167
+    API_HASH="2989ad215b7b81ec5ca936a615db51d0"
+    CASH_API_KEY=  False
+    TIME_API_KEY="UISFHDG2FJG5"
+    AI_API_KEY= False
+    WALL_API= False
+    SUPPORT_CHAT = '-1001891927588'
+    SPAMWATCH_SUPPORT_CHAT = 'SpamWatchSupport'
+    SPAMWATCH_API = 'uLHMHL2aFwsi0QRCDtzwbhKIpVBBPf487Sv4yPO9MCPXqgMR2VddxycKPc9ljVm3'
+    BL_CHATS = []
